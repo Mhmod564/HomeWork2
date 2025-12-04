826d47dd9b0e220981535c7ef1722f1f77fd6019
mahmoud <mmodmhajne@gmail.com>
2025-12-04 17:49:44 +0200

    HomeWork2(Q1)

--- src/CalculatorTCPClient.java
+++ src/CalculatorTCPClient.java
@@ -7,8 +7,9 @@ import java.net.Socket;
 public class CalculatorTCPClient {
 
 	public static void main(String[] args) {
-		try(Socket socket=new Socket("localhost",9090)){
-			System.out.println("Connected to server");
+		int port=9090;
+		try(Socket socket=new Socket("localhost",port)){
+			System.out.println("Connected to server"+port);
 			BufferedReader in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			PrintWriter out=new PrintWriter(socket.getOutputStream(),true);
 			BufferedReader stdIn=new BufferedReader(new InputStreamReader(System.in));
--- src/CalculatorTCPServer.java
+++ src/CalculatorTCPServer.java
@@ -8,13 +8,13 @@ import java.net.Socket;
 public class CalculatorTCPServer {
 
 	public static void main(String[] args) {
-		try (ServerSocket server=new ServerSocket(9090)){
-			System.out.println("Server is listening to 9090");
+		int port=9090;
+		try (ServerSocket server=new ServerSocket(port)){
+			System.out.println("Server is listening to"+port);
 			Socket s=server.accept();
 			System.out.println("client connected");
 			BufferedReader in=new BufferedReader(new InputStreamReader(s.getInputStream()));
 			PrintWriter out=new PrintWriter(s.getOutputStream(),true);
-			
 			String[]operators= {"+","-","/","*"};
 			char operator;
 			String inputLine;
