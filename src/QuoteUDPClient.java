3fbf9a7110bd7e3bed67b53ad58a7062f57bd9af
mahmoud <mmodmhajne@gmail.com>
2025-12-04 17:49:55 +0200

    HomeWork2(Q2)

--- src/QuoteUDPClient.java
+++ src/QuoteUDPClient.java
@@ -1,9 +1,52 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
 
 public class QuoteUDPClient {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 Scanner scanner = new Scanner(System.in);
+		try(DatagramSocket clientSocket = new DatagramSocket()) {
+			 InetAddress serverAddress = InetAddress.getByName("localhost");
+			 int serverPort = 8080;
+			 String clientMessage;
+			 byte[] sendBuffer;
+			 byte[] receiveBuffer = new byte[1024];
+			 System.out.println("Enter 'GET' to receive a quote:");
+			 while (true) {
+			 // Send data to server
+			 while(true) {			 
+				 clientMessage = scanner.nextLine();
+			 	if(clientMessage.equalsIgnoreCase("get")||clientMessage.equalsIgnoreCase("exit")) {
+			 		break;
+				}else System.out.println("Please enter 'GET' to receive a quote:");
+			 }
+			 
+			 sendBuffer = clientMessage.getBytes();
+			 DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress,serverPort);
+			 clientSocket.send(sendPacket);
+			 	if (clientMessage.equalsIgnoreCase("exit")) {
+				 	System.out.println("Client is shutting down...");
+				 	break;
+			 	}
+			 // Receive response from server
+			 DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+			 clientSocket.receive(receivePacket);
+			 String serverResponse = new String(receivePacket.getData(), 0, receivePacket.getLength());
+			 System.out.println("Quote received: " + serverResponse);
 
+			}
+			 } catch (Exception e) {
+			 e.printStackTrace();
+			 }
+			finally{
+				 scanner.close();
+			
+			}
+		}
+			 
 	}
 
-}
+
--- src/QuoteUDPServer.java
+++ src/QuoteUDPServer.java
@@ -1,9 +1,42 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Random;
 
 public class QuoteUDPServer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int port=8080;
+		try(DatagramSocket serverSocket = new DatagramSocket(port)) {
+			 System.out.println("Server is running on port " + port);
+			 String [] quotes= {"Choose what chooses you.","Energy doesn’t lie.","Small steps, big changes.","Your vibe writes your story.","Stay close to what feels like sunshine."};
+		     Random rand = new Random();
+			 byte[] receiveBuffer = new byte[1024];
+			 byte[] sendBuffer;
+			 while (true) {
+			     int index = rand.nextInt(quotes.length);
+			 // Receive data from client
+			 DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+			 serverSocket.receive(receivePacket);
+			 String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+			 System.out.println("Received: " + clientMessage);
+			 if (clientMessage.equalsIgnoreCase("exit")) {
+			 System.out.println("Server is shutting down...");
+			 break;
+			 }
+			 // Send response to client
+			 String serverResponse = quotes[index];
+			 sendBuffer = serverResponse.getBytes();
+			 InetAddress clientAddress = receivePacket.getAddress();
+			 int clientPort = receivePacket.getPort();
+			 DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, clientAddress,clientPort);
+			 serverSocket.send(sendPacket);
+			 }
+			 } catch (Exception e) {
+			 e.printStackTrace();
+			 }
+			 
 	}
 
 }
